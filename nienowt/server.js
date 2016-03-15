97c3dd9461bc230e4681641f45e09fcae7005354
Thom Nienow <elstad.nienow@gmail.com>
2016-03-15 11:35:52 -0700

    tests pass, added gulp/package

--- nienowt/server.js
+++ nienowt/server.js
@@ -2,52 +2,32 @@
 
 let express = require('express');
 let app = express();
-let shoutRouter = express.Router();
 
-let db = {
-  screams : [
-    {
-      id:0,
-      name:'AHh!'
-    },
-    {
-      id:1,
-      name:'DAAA!'
+app.use((req, res, next) => {
+  req.body = [];
+  if(req.method === 'GET') return next();
+  req.on('data',(chunk) => {
+    req.body.push(chunk);
+  }).on('end', function(){
+    try {
+      req.body = JSON.parse(Buffer.concat(req.body).toString());
+      console.log(req.body);
+      next();
+    } catch (e){
+      res.status(400).send('Invalid json');
+      return res.end();
     }
-  ]
-}
+  });
+});
 
-
-app.use('/screams',(req, res, next) => {
- req.body = [];
- if(req.method === 'GET') return next();
- req.on('data',(chunk) => {
-   req.body.push(chunk)
-   console.log('before' , chunk)
- }).on('end', function(err){
-   if(err) return next()
-   req.body = JSON.parse(Buffer.concat(req.body).toString());
-   console.log(typeof req.body)
-   next();
-   })
- })
-
-shoutRouter.route('/screams')
-.get((req,res) => {
-  res.json({
-    status: true,
-    data: db.screams
-  })
-})
-  .post((req, res) =>{
-    let scream = req.body;
-    db.screams.push(req.body)
-    console.log(req.body)
-    res.end();
-  })
-
-
-app.use('/', shoutRouter)
-
+app.post('/screams',(req, res) =>{
+  if(req.body){
+    var key = Object.keys(req.body)[0];
+    var value = req.body[key];
+    var keyValue = key + ': ' + value;
+    res.write(keyValue);
+  }
+  res.end();
+});
 
 app.listen(3000);
---
+++ nienowt/test/test.js
@@ -0,0 +1,32 @@
+'use strict';
+
+var chai = require('chai');
+var chaiHTTP = require('chai-http');
+chai.use(chaiHTTP);
+var request = chai.request;
+var expect = chai.expect;
+require(__dirname + '/../server.js');
+
+
+describe('parse middleware', () => {
+  it('should parse incoming json, write to req.body', (done) => {
+    request('localhost:3000')
+    .post('/screams')
+    .send('{"what":"AH!"}')
+    .end((err, res) => {
+      expect(err).to.eql(null);
+      expect(res.text).to.eql('what: AH!');
+      done();
+    });
+  });
+  it('should respond "invalid json"', (done) => {
+    request('localhost:3000')
+    .post('/screams')
+    .send('mk')
+    .end((err, res) => {
+      expect(res).to.have.status(400);
+      expect(res.text).to.eql('Invalid json');
+      done();
+    });
+  });
+});
