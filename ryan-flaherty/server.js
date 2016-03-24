586478dd4862eff417820d06a0ac4312a8413673
Ryan Flaherty <rcflaherty@gmail.com>
2016-03-24 10:10:41 -0700

    middleware and tests working

--- ryan-flaherty/server.js
+++ ryan-flaherty/server.js
@@ -7,11 +7,12 @@ var port = process.env.PORT || 3000;
 var parserfy = require(__dirname + '/lib/parserfy');
 
 app.post('/post', parserfy, (req, res) => {
-  res.send(res.body);
+  res.send(req.body);
   res.end();
 });
 
 app.listen(port, function() {
   console.log('Server listening on port ' + (port || 3000));
 });
+
 
